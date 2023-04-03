c7401d0fefa1e51dc7a28f3b7124b8101aafe4f3
khg <winquake3@hanmail.net>
2023-04-04 00:32:02 +0900

    DataSource 구성

--- src/test/java/org/zerock/springex/sample/SampleTests.java
+++ src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @Log4j2
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
@@ -15,9 +18,20 @@ public class SampleTests {
     @Autowired //스프링에서 사용하는 의존성 주입 어노테이션 (빈이 존재하면 여기에 주입해라)
     private SampleService sampleService;
 
+    @Autowired
+    private DataSource dataSource;
     @Test
     public void testSerbice1() {
         log.info(sampleService);
         Assertions.assertNotNull(sampleService);
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+        Assertions.assertNotNull(connection);
+
+        connection.close();
     }
 }
